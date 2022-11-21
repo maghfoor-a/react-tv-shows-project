c12286b23d8245f5d487bcbc5546e6e29f3f3ccd
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-21 15:33:31 +0000

    Write SingleEpisodeView component

--- src/components/SingleEpisodeView.tsx
+++ src/components/SingleEpisodeView.tsx
@@ -1,11 +1,14 @@
 import episodeCode from "../utils/episodeCode";
 import { IEpisode } from "../episodesInterface";
 
-function singleEpisodeView(episode: IEpisode): JSX.Element {
+export default function singleEpisodeView(episode: IEpisode): JSX.Element {
     const episodeCodeName = episodeCode(episode);
     return (
         <>
-            
+            <h3>{episodeCodeName}: <a href={episode.url}>{episode.name}</a></h3>
+            <h5>Rating: {episode.rating.average}</h5>
+            <img className="episodeImg" src={episode.image.medium} alt={episode.name}/>
+            <p>{episode.summary}</p>
         </>
     )
 }
