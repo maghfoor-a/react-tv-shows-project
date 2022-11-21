4445b87b5f2d8c2cd21d9a7b3efca934c94d2ad1
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-21 16:26:21 +0000

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import AppHeader from "./components/AppHeader";
 import EpisodesView from "./components/AllEpisodesView";
+import Footer from "./components/AppFooter";
 function App(): JSX.Element {
   return (
     <>
       <AppHeader />
       <EpisodesView />
+      <hr/>
+      <Footer />
     </>
   );
 }
---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,7 @@
+export default function Footer(): JSX.Element {
+    return (
+        <>
+            <p>THIS DATA WAS OBTAINED FROM: <a href="TVMaze.com" target="_blank" rel="noopener noreferrer">TVMaze.com</a>. NO COPYRIGHT INFRINGEMENTS INTENDED.</p>
+        </>
+    )
+}
