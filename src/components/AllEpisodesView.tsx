c78948bffd6f4afbb5714c15aff7d768053b7a5a
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-21 17:33:38 +0000

    ran yarn format

--- src/components/AllEpisodesView.tsx
+++ src/components/AllEpisodesView.tsx
@@ -4,17 +4,27 @@ import SearchBar from "./SearchBar";
 import { useState } from "react";
 
 export default function EpisodesView(): JSX.Element {
-
-  const [searchBarText, setSearchBarText] = useState<string>("")
+  const [searchBarText, setSearchBarText] = useState<string>("");
   return (
     <>
       <h1>EPISODES</h1>
-      <SearchBar searchBarText={searchBarText} setSearchBarText={setSearchBarText}/>
+      <SearchBar
+        searchBarText={searchBarText}
+        setSearchBarText={setSearchBarText}
+      />
       {episodes
-      .filter(episode => episode.name.toLocaleLowerCase().includes(searchBarText.toLocaleLowerCase())||episode.summary.toLocaleLowerCase().includes(searchBarText.toLocaleLowerCase()))
-      .map((episode) => {
-        return <SingleEpisodeView key={episode.id} episode={episode} />;
-      })}
+        .filter(
+          (episode) =>
+            episode.name
+              .toLocaleLowerCase()
+              .includes(searchBarText.toLocaleLowerCase()) ||
+            episode.summary
+              .toLocaleLowerCase()
+              .includes(searchBarText.toLocaleLowerCase())
+        )
+        .map((episode) => {
+          return <SingleEpisodeView key={episode.id} episode={episode} />;
+        })}
     </>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,13 @@
-
 interface SearchBarProps {
-    searchBarText: string,
-    setSearchBarText: React.Dispatch<React.SetStateAction<string>>
+  searchBarText: string;
+  setSearchBarText: React.Dispatch<React.SetStateAction<string>>;
 }
 
-
 export default function SearchBar(props: SearchBarProps): JSX.Element {
-
-    
-    return (
-        <input value={props.searchBarText} onChange={(event) => props.setSearchBarText(event.target.value)}></input>
-    )
+  return (
+    <input
+      value={props.searchBarText}
+      onChange={(event) => props.setSearchBarText(event.target.value)}
+    ></input>
+  );
 }
