3074307d8c11754e5ba1dfffe6c49ff2fcae73d7
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-21 16:15:49 +0000

    format AGAIN

--- src/components/AllEpisodesView.tsx
+++ src/components/AllEpisodesView.tsx
@@ -1,14 +1,13 @@
-import episodes from '../episodes.json';
-import SingleEpisodeView from './SingleEpisodeView';
+import episodes from "../episodes.json";
+import SingleEpisodeView from "./SingleEpisodeView";
 
 export default function EpisodesView(): JSX.Element {
-
-    return (
-        <>
-            <h1>EPISODES</h1>
-            {episodes.map((episode) => {
-            return <SingleEpisodeView key={episode.id} episode={episode}/>
-        })}
-        </>
-    )
+  return (
+    <>
+      <h1>EPISODES</h1>
+      {episodes.map((episode) => {
+        return <SingleEpisodeView key={episode.id} episode={episode} />;
+      })}
+    </>
+  );
 }
