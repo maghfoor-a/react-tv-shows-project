77c1e31eff1a72f57aa27778cd70bb09ce88419f
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-21 14:37:55 +0000

    made a rough draft of SingleepisodeView

--- src/components/SingleEpisodeView.tsx
+++ src/components/SingleEpisodeView.tsx
@@ -1,4 +1,11 @@
 import episodeCode from "../utils/episodeCode";
 import { IEpisode } from "../episodesInterface";
 
-function singleEpisodeView()
+function singleEpisodeView(episode: IEpisode): JSX.Element {
+    const episodeCodeName = episodeCode(episode);
+    return (
+        <>
+            
+        </>
+    )
+}
