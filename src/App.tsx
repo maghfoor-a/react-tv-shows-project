3e0a1f42cec2907e186f5f00841235e40777c1db
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-21 11:40:19 +0000

    added interface for episodes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { greet } from "./utils/greet";
 import episodes from './episodes.json'
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  console.log(`Imported ${episodes.length} episode(s)`);
+  console.log(`First episode's name is ${episodes[0].name}`);
+  return (
+  <h1>{greet("World")}</h1>
+  )
 }
 
 export default App;
