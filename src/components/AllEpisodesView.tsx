8db3884bf6995d47ba9e537c77768ad28eb79b84
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-21 16:11:14 +0000

    Write map function to show all singleEpisode elements

--- src/components/AllEpisodesView.tsx
+++ src/components/AllEpisodesView.tsx
@@ -1,8 +1,14 @@
 import episodes from '../episodes.json';
-import Single
+import SingleEpisodeView from './SingleEpisodeView';
 
 export default function EpisodesView(): JSX.Element {
+
     return (
-        <h1>EPISODES</h1>
+        <>
+            <h1>EPISODES</h1>
+            {episodes.map((episode) => {
+            return <SingleEpisodeView key={episode.id} episode={episode}/>
+        })}
+        </>
     )
 }
--- src/components/SingleEpisodeView.tsx
+++ src/components/SingleEpisodeView.tsx
@@ -1,14 +1,21 @@
 import episodeCode from "../utils/episodeCode";
 import { IEpisode } from "../episodesInterface";
 
-export default function singleEpisodeView(episode: IEpisode): JSX.Element {
+interface EpisodeProps {
+    episode: IEpisode
+}
+
+
+export default function SingleEpisodeView(props: EpisodeProps): JSX.Element {
+    const episode = props.episode
     const episodeCodeName = episodeCode(episode);
+
     return (
-        <>
-            <h3>{episodeCodeName}: <a href={episode.url}>{episode.name}</a></h3>
+        <div className="singleEpisode">
+            <h3>{episodeCodeName}: <a href={episode.url} target="_blank" rel="noopener noreferrer">{episode.name}</a></h3>
             <h5>Rating: {episode.rating.average}</h5>
             <img className="episodeImg" src={episode.image.medium} alt={episode.name}/>
             <p>{episode.summary}</p>
-        </>
+        </div>
     )
 }
