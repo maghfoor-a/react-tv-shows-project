45be2d82eea509bf801db9959a6fec6c78a74559
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-22 15:25:34 +0000

    made sort function

---
+++ src/utils/sortAlphabetically.ts
@@ -0,0 +1,5 @@
+import { IShow } from "./AllShowsInterface";
+
+export default function sortAlphabetically(allShows: IShow[]) {
+    return allShows.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+}
