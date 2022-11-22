8da4a79a146c84382985e8b80e6c6e49131e8b76
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-22 11:24:07 +0000

    trying fetch

--- src/components/AllEpisodesView.tsx
+++ src/components/AllEpisodesView.tsx
@@ -1,13 +1,21 @@
 import simpsonsEpisodes from "../simpsonsEpisodes.json";
 import SingleEpisodeView from "./SingleEpisodeView";
 import SearchBar from "./SearchBar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import searchFilter from "../utils/searchFilter";
 
 export default function EpisodesView(): JSX.Element {
   const [searchBarText, setSearchBarText] = useState<string>("");
 
   const filteredEpisodes = searchFilter(simpsonsEpisodes, searchBarText);
+
+
+      useEffect(() => {
+      const response = await fetch("https://api.tvmaze.com/shows/82/episodes")
+      const jsonBody = await response.json()
+      const allEpsiodes = jsonBody
+
+    }, [])
 
   return (
     <>
