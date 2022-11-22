ccefb1568640dc4a6a5d3037b3ac0ad766d1deb4
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-22 14:56:26 +0000

    implemented map function

--- src/components/AllEpisodesView.tsx
+++ src/components/AllEpisodesView.tsx
@@ -3,14 +3,28 @@ import SearchBar from "./SearchBar";
 import { useState, useEffect } from "react";
 import searchFilter from "../utils/searchFilter";
 import { IEpisode } from "../episodesInterface";
+import { IShow } from "../utils/AllShowsInterface";
 
 export default function EpisodesView(): JSX.Element {
   const [searchBarText, setSearchBarText] = useState<string>("");
   const [allEpisodes, setAllEpisodes] = useState<IEpisode[] | []>([]);
+  const [allShows, setAllShows] = useState<IShow[] | []>([]);
+  const [showID, setShowID] = useState<number>(1);
+
+  useEffect(() => {
+    const fetchAllShows = async () => {
+      const response = await fetch("http://api.tvmaze.com/shows?page=1");
+      const jsonBody = await response.json();
+      setAllShows(jsonBody);
+    };
+    fetchAllShows();
+  }, []);
 
   useEffect(() => {
     const fetchAllEpisodes = async () => {
-      const response = await fetch("https://api.tvmaze.com/shows/5/episodes");
+      const response = await fetch(
+        `https://api.tvmaze.com/shows/${showID}/episodes`
+      );
       const jsonBody = await response.json();
       setAllEpisodes(jsonBody);
     };
@@ -21,6 +35,13 @@ export default function EpisodesView(): JSX.Element {
 
   return (
     <>
+      <select onChange={(event) => console.log(event.target.value)}>
+        {allShows.map((show) => (
+          <option value={show.id} key={show.id}>
+            {show.name}
+          </option>
+        ))}
+      </select>
       <h1>EPISODES</h1>
       <SearchBar
         searchBarText={searchBarText}
