aab992661357d69974afb9c209cb5f00f7624677
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-24 12:39:29 +0000

    change to AllShowView

--- src/components/AllShowsView.tsx
+++ src/components/AllShowsView.tsx
@@ -6,9 +6,13 @@ interface AllShowsViewProps {
 }
 
 export default function AllShowsView(props: AllShowsViewProps): JSX.Element {
+    const allShows = props.allShows
     return (
         <>
             <h1>ALL SHOWS</h1>
+            {allShows.map(show => {
+                return <SingleShowView key={show.id} show={show}/>
+            })}
         </>
     )
 }
