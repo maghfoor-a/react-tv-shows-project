b347c90dad35ad246e7a8f9405acfe49c1104c86
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-25 12:45:32 +0000

    dropdown menu to select seasons, but still has a bug

--- src/utils/CreateSeasonsArray.ts
+++ src/utils/CreateSeasonsArray.ts
@@ -1,7 +1,7 @@
 export default function createSeasonsArray(number: number): number[] {
-    const seasonsArray = []
-    for(let i=1; i<=number; i++){
-        seasonsArray.push(i)
-    }
-    return seasonsArray
+  const seasonsArray = [];
+  for (let i = 1; i <= number; i++) {
+    seasonsArray.push(i);
+  }
+  return seasonsArray;
 }
